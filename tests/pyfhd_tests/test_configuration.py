dcc6df22c08020f8c72cb1b716a01ead0181ba4f
SkyWa7ch3r <joel.g.w.dunstan@gmail.com>
2025-06-17 18:29:52 +0800

    Made config test

---
+++ tests/pyfhd_tests/test_configuration.py
@@ -0,0 +1,39 @@
+from logging import Logger
+from PyFHD.pyfhd_tools.pyfhd_setup import pyfhd_parser, pyfhd_setup
+import sys
+import importlib_resources
+import configargparse
+
+
+def test_configuration():
+    """
+    Test the configuration setup for PyFHD.
+    This function checks if the configuration parser is correctly initialized.
+    """
+    sys.argv = [
+        "pyfhd",
+        "--config",
+        str(
+            importlib_resources.files("PyFHD").joinpath(
+                "resources/1088285600_example/1088285600_example.yaml"
+            )
+        ),
+        "--silent",
+        "--no-log-file",
+        "1088285600",
+    ]
+    # Initialize the configuration parser
+    configargparser = pyfhd_parser()
+    options = configargparser.parse_args()
+    pyfhd_config, logger = pyfhd_setup(options)
+
+    # Check if the parser is an instance of ArgumentParser
+    assert isinstance(configargparser, configargparse.ArgumentParser)
+    assert isinstance(pyfhd_config, dict)
+    assert "obs_id" in pyfhd_config
+    assert pyfhd_config["obs_id"] == "1088285600"
+    assert "silent" in pyfhd_config
+    assert pyfhd_config["silent"] is True
+    assert "log_file" in pyfhd_config
+    assert pyfhd_config["log_file"] is False
+    assert isinstance(logger, Logger)
